b3b45de9a63284689ea8d504a23c88d408bd7136
Ænthroppe <jamison.bassett@gmail.com>
2025-09-18 21:58:23 +0200

    enabled time for kaleidoscope

--- autism-vn/mandelbrot.gd
+++ autism-vn/mandelbrot.gd
@@ -7,7 +7,22 @@ var time = 0.0
 var power = 0.1
 var zoom = 2.0
 var zoom_in = true
+
+var count_time := true
+var shader_time := 0.0
+#func _input(event):
+	#if(event.is_action("ui_accept")):
+		#count_time = false
+		##SceneTransition.change_scene(target_scene)
+
+func set_shader_time(new_shader_time: float):
+	print(new_shader_time)
+	self.material.set_shader_parameter("shader_time", new_shader_time)
+	
 func _process(delta):
+	if count_time:
+		shader_time += delta
+	set_shader_time(shader_time)
 	if(!enable):
 		return
 	
